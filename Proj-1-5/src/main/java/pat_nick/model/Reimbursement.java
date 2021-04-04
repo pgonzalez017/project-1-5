1d6cc8f904135a847abe8b183fb5de7286ae3b31
Patrick Gonzalez <pgonzalez017@gmail.com>
2021-04-04 17:46:50 -0400

    Packages and Models/Pom

---
+++ Proj-1-5/src/main/java/pat_nick/model/Reimbursement.java
@@ -0,0 +1,4 @@
+package pat_nick.model;
+
+public class Reimbursement {
+}
---
+++ Proj-1-5/src/main/java/pat_nick/model/User.java
@@ -0,0 +1,168 @@
+package pat_nick.model;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NamedQueries({@NamedQuery(name="getByUsername", query="from User where id = :id"),
+        @NamedQuery(name="getAllUsers", query = "from User")})
+@Entity(name="User")
+@Table(name = "ers_users")
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private String firstname;
+    @Column
+    private String lastname;
+    @Column
+    private String email;
+    @Column
+    private int role_id;
+
+    @OneToMany(mappedBy = "user",
+            orphanRemoval = true,
+            fetch = FetchType.EAGER)
+    private List<Reimbursement> reimbursements;
+
+    public User(String username, String password, String firstname, String lastname, String email, int role_id) {
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.role_id = role_id;
+        reimbursements = new ArrayList<>();
+    }
+
+
+    public User() {
+        //No-arg constructor
+        reimbursements = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+
+    public List<Reimbursement> getReimbursements() { return reimbursements; }
+
+    public void setReimbursements(List<Reimbursement> reimbursements) { this.reimbursements = reimbursements; }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+        result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        result = prime * result + role_id;
+        result = prime * result + id;
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (firstname == null) {
+            if (other.firstname != null)
+                return false;
+        } else if (!firstname.equals(other.firstname))
+            return false;
+        if (lastname == null) {
+            if (other.lastname != null)
+                return false;
+        } else if (!lastname.equals(other.lastname))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        if (role_id != other.role_id)
+            return false;
+        if (id != other.id)
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", username=" + username + ", password=" + password + ", firstname="
+                + firstname + ", lastname=" + lastname + ", email=" + email + ", role_id=" + role_id + "]";
+    }
+}
