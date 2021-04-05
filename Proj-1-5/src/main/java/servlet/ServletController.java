f5bb8bb3206b90c383a3ab1a9a3cce1b9d0c55ff
Nick Z <nicolas.zimmerman@revature.net>
2021-04-04 19:50:36 -0600

    Added files for super basic srevlet

---
+++ Proj-1-5/src/main/java/servlet/ServletController.java
@@ -0,0 +1,41 @@
+package servlet;
+
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+public class ServletController extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //ResultSet rs = Dao.dispAll();
+        ArrayList<String> allUsers = new ArrayList<>();
+
+        resp.getWriter().println("<h1>welcome to servlets</h1>");
+    }
+
+}
